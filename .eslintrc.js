bb3cf24134d1905f085b70f40f3d8e14a1d38aa8
magicyu90 <magicyu90@gmail.com>
2017-08-22 00:13:59 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
--- index.js
+++ index.js
@@ -6,13 +6,14 @@ import config from './config'
 const app = new Koa();
 
 mongoose.Promise = global.Promise
-mongoose.connect(config.database)
+
+mongoose.createConnection(config.database)
 
 app.use(bodyParser);
 
 app.use(async(ctx) => {
-    ctx.body = "Hello Hugo";
-});
+    ctx.body = "Hello Hugo"
+})
 
-app.listen(3000);
+app.listen(3000)
 console.log('[demo] start-quick is starting at port 3000')
